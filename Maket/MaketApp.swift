90481d7e90664f1e38a81fc5ce24fcbc3cd09363
itsmylifestyle <hahaii00@mail.ru>
2023-01-21 23:39:18 +0600

    Initial Commit

---
+++ Maket/MaketApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaketApp.swift
+//  Maket
+//
+//  Created by Айбек on 21.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct MaketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
