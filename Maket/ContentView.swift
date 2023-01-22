ada28617b5b3f5e14eb160f9bbb17834d38fc635
itsmylifestyle <hahaii00@mail.ru>
2023-01-23 00:40:19 +0600

    Trying UserDefaults setting through different classes

--- Maket/ContentView.swift
+++ Maket/ContentView.swift
@@ -9,35 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var message = ""
-    @State var get = ""
-    var color = #colorLiteral(red: 0, green: 0.8761811852, blue: 0, alpha: 1)
+    @ObservedObject var settings = UDSettings()
     
     var body: some View {
-        VStack {
-            Text(get)
-            TextField("Enter your message", text: $message)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-            
-            Button(action: {
-                UserDefaults.standard.set(self.message, forKey: "message")
-                self.get = self.message
-                self.message = ""
-            }) {
-                Text("Save")
-                    .padding()
-                    .padding(.horizontal, 25)
+        VStack(alignment: .center) {
+            Toggle(isOn: self.$settings.isOn) {
+                Text("")
             }
-            .background(Color(color))
-            .foregroundColor(.white)
-            .cornerRadius(10)
+            .fixedSize()
         }
-        .onAppear {
-            guard let gotMessage = UserDefaults.standard.value(forKey: "message") else { return }
-            self.get = gotMessage as! String
-            //as! = typecasting
-        }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+        .background(settings.isOn ? Color.green : Color.blue)
     }
 }
 
