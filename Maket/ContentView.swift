ccc2e64704cceb5d7bf3984b696b1ba7ecb12938
itsmylifestyle <hahaii00@mail.ru>
2023-01-23 00:27:42 +0600

    UserDefault practice

--- Maket/ContentView.swift
+++ Maket/ContentView.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var message = ""
+    @State var get = ""
+    var color = #colorLiteral(red: 0, green: 0.8761811852, blue: 0, alpha: 1)
+    
     var body: some View {
-        Text("Hello")
-            .padding()
+        VStack {
+            Text(get)
+            TextField("Enter your message", text: $message)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            Button(action: {
+                UserDefaults.standard.set(self.message, forKey: "message")
+                self.get = self.message
+                self.message = ""
+            }) {
+                Text("Save")
+                    .padding()
+                    .padding(.horizontal, 25)
+            }
+            .background(Color(color))
+            .foregroundColor(.white)
+            .cornerRadius(10)
+        }
+        .onAppear {
+            guard let gotMessage = UserDefaults.standard.value(forKey: "message") else { return }
+            self.get = gotMessage as! String
+            //as! = typecasting
+        }
     }
 }
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
